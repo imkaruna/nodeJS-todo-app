117e1a85c9890c7c3ab727863e84349c3209ecc3
Karuna <imkaruna@gmail.com>
2017-02-08 22:41:35 -0500

    Updated app.js with controllers, express and template file setup

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var http = require ('http');
+var express = require('express');
+var todoController = require('./controllers/mainController.js');
+var app = express();
+
+//Setup template engine
+app.set('view engine', 'ejs');
+
+
+// Setup static files
+app.use(express.static('./public'));
+
+todoController(app);
+// Port the app will be running on
+app.listen(3000);
+console.log('Now listening at port 3000...');
